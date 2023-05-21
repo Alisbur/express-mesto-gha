56ac2b6270df58efe17510ec4e3a027e81df59d1
Alisbur <mail-4-eugen@yandex.ru>
2023-05-22 02:57:51 +0300

    final v1.1

--- controllers/card.js
+++ controllers/card.js
@@ -1,22 +1,37 @@
 const Card = require('../models/card');
 
+const VALIDATION_ERROR_CODE = 400;
+const VALIDATION_ERROR_MESSAGE = { message: `переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля`};
+const NOT_FOUND_ERROR_CODE = 404;
+const NOT_FOUND_ERROR_MESSAGE = { message: `карточка или пользователь не найден`};
+const DEFAULT_ERROR_CODE = 500;
+const DEFAULT_ERROR_MESSAGE = { message: `ошибка по-умолчанию`};
+
 const getAllCards = (req, res) => {
   Card.find({})
     .then(data => res.status(200).send({ data: data }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err => res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE));
 }
 
 const deleteCardById = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then(card => res.status(200).send({ data: card }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err => {
+      err.name==="CastError"
+        ? res.status(NOT_FOUND_ERROR_CODE).send(NOT_FOUND_ERROR_MESSAGE)
+        : res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE);
+    });
 }
 
 const createCard = (req, res) => {
   const body = {...req.body, ["owner"]:req.user._id};
   Card.create(body)
     .then(card => res.status(201).send({ data: card }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err => {
+      err.name==="ValidationError"
+        ? res.status(VALIDATION_ERROR_CODE).send(VALIDATION_ERROR_MESSAGE)
+        : res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE);
+    });
 }
 
 const addLike = (req, res) => {
@@ -25,7 +40,11 @@ const addLike = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true })
     .then(card => res.status(201).send({ data: card }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err => {
+      err.name==="CastError"
+        ? res.status(NOT_FOUND_ERROR_CODE).send(NOT_FOUND_ERROR_MESSAGE)
+        : res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE);
+    });
 };
 
 const removeLike = (req, res) => {
@@ -34,7 +53,12 @@ const removeLike = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true })
     .then(card => res.status(201).send({ data: card }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err => {
+      console.log(err.name);
+      err.name==="CastError"
+        ? res.status(NOT_FOUND_ERROR_CODE).send(NOT_FOUND_ERROR_MESSAGE)
+        : res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE);
+    });
 };
 
 module.exports = {getAllCards, deleteCardById, createCard, addLike, removeLike};
--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,36 @@
 const User = require('../models/user');
 
+const VALIDATION_ERROR_CODE = 400;
+const VALIDATION_ERROR_MESSAGE = { message: `переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля`};
+const NOT_FOUND_ERROR_CODE = 404;
+const NOT_FOUND_ERROR_MESSAGE = { message: `карточка или пользователь не найден`};
+const DEFAULT_ERROR_CODE = 500;
+const DEFAULT_ERROR_MESSAGE = { message: `ошибка по-умолчанию`};
+
 const getAllUsers = (req, res) => {
   User.find({})
     .then(data => res.status(200).send({ data: data }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err => res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE));
 }
 
 const getUserById = (req, res) => {
   User.findById(req.params.id)
     .then(data => res.status(200).send({ data: data }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err => {
+      err.name==="CastError"
+        ? res.status(NOT_FOUND_ERROR_CODE).send(NOT_FOUND_ERROR_MESSAGE)
+        : res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE);
+    });
 }
 
 const createUser = (req, res) => {
   User.create(req.body)
     .then(data => res.status(201).send({ data: data }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err => {
+      err.name==="ValidationError"
+        ? res.status(VALIDATION_ERROR_CODE).send(VALIDATION_ERROR_MESSAGE)
+        : res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE);
+    });
 }
 
 const updateProfile = (req, res) => {
@@ -35,7 +50,12 @@ const updateProfile = (req, res) => {
         upsert: true
     })
     .then(data => res.status(201).send({ data: data }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err =>
+      err.name==="CastError"
+        ? res.status(NOT_FOUND_ERROR_CODE).send(NOT_FOUND_ERROR_MESSAGE)
+        : err.name==="ValidationError"
+          ? res.status(VALIDATION_ERROR_CODE).send(VALIDATION_ERROR_MESSAGE)
+          : res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE));
 }
 
 const updateAvatar = (req, res) => {
@@ -53,7 +73,12 @@ const updateAvatar = (req, res) => {
         upsert: true
     })
     .then(data => res.status(201).send({ data: data }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch(err =>
+      err.name==="CastError"
+        ? res.status(NOT_FOUND_ERROR_CODE).send(NOT_FOUND_ERROR_MESSAGE)
+        : err.name==="ValidationError"
+          ? res.status(VALIDATION_ERROR_CODE).send(VALIDATION_ERROR_MESSAGE)
+          : res.status(DEFAULT_ERROR_CODE).send(DEFAULT_ERROR_MESSAGE));
 }
 
 module.exports = {getAllUsers, getUserById, createUser, updateProfile, updateAvatar};
