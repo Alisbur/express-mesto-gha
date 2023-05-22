02961ea298bf960b6f47d7ac9cbe82e2387b273a
Alisbur <mail-4-eugen@yandex.ru>
2023-05-22 04:07:42 +0300

    final v1.4

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,22 @@ const usersRouter = require('express').Router();
 const User = require('../models/user');
 const userController = require('../controllers/user');
 
-/*const doesIdExist = (req, res, next) => {
-  if (!User[req.params._id]) {
-    res.status(404).send(`MidleWare говорит Такого пользователя не существует`);
+const VALIDATION_ERROR_CODE = 400;
+const VALIDATION_ERROR_MESSAGE = { message: `переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля`};
+
+const regexp = new RegExp(/[0-9a-z]{24}/);
+
+const checkId = (req, res, next) => {
+  if (!regexp.test(req.params._id)) {
+    res.status(VALIDATION_ERROR_CODE).send(VALIDATION_ERROR_MESSAGE);
     return;
   }
 
   next();
-};*/
+};
 
 usersRouter.get('/', userController.getAllUsers);
-/*usersRouter.get('/:id', doesIdExist);*/
+usersRouter.get('/:id', checkId);
 usersRouter.get('/:id', userController.getUserById);
 usersRouter.post('/', userController.createUser);
 usersRouter.patch('/me', userController.updateProfile);
